3af1323012a1e8439e38325cac03f4049c12d98b
AntKrai <antonkraevskij@gmail.com>
2025-11-01 23:59:51 +0100

    Fix foooter dimensions

--- frontend-hrow/src/App.tsx
+++ frontend-hrow/src/App.tsx
@@ -467,8 +467,8 @@ export default function TableWithToolbar() {
         />
       )}
 
-      {/*<footer>
-         <div>
+      {/* <footer>
+        <div>
           Made with <span aria-label="love">❤️</span> by Solvro © 2025
         </div>
         <a href="mailto:kn.solvro@pwr.edu.pl">kn.solvro@pwr.edu.pl</a>
